10fa3d42661b89211fc932c188c527df8aac15ef
StefanoWP <StefanoWP@users.noreply.github.com>
2019-10-15 16:09:45 +0100

    Create wp-machina-mu.php

---
+++ wp-machina-mu.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: WP-Machina MU Plugin
+Plugin URI: https://stefanowp.com
+Description: A set of functions I use on all sites while building
+Author: Stefano Grammenos
+Version: 0.0.1
+Requires at least: 3.0
+Author URI: https://stefano.com
+*/
+
+/**
+ * Set up our various actions
+ */

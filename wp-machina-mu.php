65d07a999f06981702a9b5cab38803f09e42526f
StefanoWP <StefanoWP@users.noreply.github.com>
2019-10-15 16:12:52 +0100

    Update wp-machina-mu.php

--- wp-machina-mu.php
+++ wp-machina-mu.php
@@ -12,3 +12,32 @@ Author URI: https://stefano.com
 /**
  * Set up our various actions
  */
+
+// Redefine the "expensive" query for Query Monitor.
+define( 'QM_DB_EXPENSIVE', 0.1 );
+
+
+/**
+ * Remove all dashboard widgets.
+ *
+ * @global $wp_meta_boxes
+ */
+add_action( 'wp_dashboard_setup', function () {
+	global $wp_meta_boxes;
+	// Clear out the dashboard meta boxes array.
+	$wp_meta_boxes['dashboard'] = array();
+}, 999 );
+/**
+ * Add some custom CSS to the admin dashboard to clean up the presentation.
+ *
+ * This will hide the "Drag Boxes Here" message, as there are no boxes to drag.
+ */
+add_action( 'admin_print_styles-index.php', function () {
+?>
+
+    <style type="text/css">
+        #dashboard-widgets-wrap .empty-container { display: none; }
+    </style>
+
+<?php
+} );
